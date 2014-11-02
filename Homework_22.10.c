f3bc94bfbb5b55728c62fc52036b871c8a7f4fb0
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-02 19:33:15 +0400

    Update Homework_22.10.c

--- Homework_22.10.c
+++ Homework_22.10.c
@@ -1,5 +1,15 @@
+//двоичное представление числа
+
 #include <stdio.h>
 main()
 {
-
+ const int s = sizeof(int) * 8;
+ int n=0;
+ scanf ("%d", &n);
+ for (int i = 0; i<s; i++)
+ {
+ printf ("%d", (n << s - 1) & 1);
+ n = n << 1;
+ }
+ 
 }
