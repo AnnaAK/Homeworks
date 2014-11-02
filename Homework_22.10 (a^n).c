abf055468264cc95fe3947d5e3f413e132fef218
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-02 21:05:37 +0400

    Update Homework_22.10 (a^n).c

--- Homework_22.10 (a^n).c
+++ Homework_22.10 (a^n).c
@@ -1,2 +1,28 @@
 //возведение в степень через битовые операции
 
+#include <stdio.h>
+
+  float stepen (float a,int n)
+  {
+    int count = 1;
+    while (n)
+    if (n%2 != 0)
+    {
+      count *= a;
+      --n;
+    }
+    else
+    {
+    a *= a;
+    n >>= 1;
+    }
+    return count;
+  }
+int main ()
+{
+int a = 0, n = 0;
+scanf ("%d", a);
+scanf ("%d", n);
+printf ("%d\n", stepen (a,n));
+return 0;
+}
