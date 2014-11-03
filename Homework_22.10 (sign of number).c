b4baf6a896bba7f8193c18ff41e473dfe429e859
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-03 15:33:55 +0400

    Update Homework_22.10 (sign of number).c

--- Homework_22.10 (sign of number).c
+++ Homework_22.10 (sign of number).c
@@ -1,1 +1,17 @@
 // вывести знак числа через битовые операции
+
+#include <stdio.h>
+int sign (int n)
+{
+    const int m = sizeof (int) * 8 - 1 ;
+   int z = 0;
+   z = (n >> m) + !(n >> m) + ~(!n + ~0);
+   return z;
+}
+int main ()
+{
+  int n;
+  scanf ("%d", &n);
+  printf("%d", sign(n));
+  return 0;
+}
