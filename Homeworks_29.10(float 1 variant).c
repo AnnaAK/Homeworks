85b35acbb31551419e941aaea93ef2ed6e90f4e0
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-04 16:41:10 +0400

    Update Homeworks_29.10(float 1 variant).c

--- Homeworks_29.10(float 1 variant).c
+++ Homeworks_29.10(float 1 variant).c
@@ -1,6 +1,6 @@
 //вывести  знак, экспоненту и мантиссу
 //через указатель
-
+#include <stdio.h>
 struct floatstruct
 {
   int sign;
@@ -8,6 +8,7 @@ struct floatstruct
   int mantissa;
 } f;
  void presentation (floatstruct)
+ #define max 255
 {
   if (f.sign)
   f.sing = -1;
@@ -16,24 +17,25 @@ struct floatstruct
   if (f.exponenta == 0 && f.mantissa == 0)
     printf ("0\n");
   else
-  if (f.exponenta == 255 && f.mantissa == 0)
+  if (f.exponenta == max && f.mantissa == 0)
   if (f.sign>0)
     printf ("sign = +");
   else
     printf ("sign = -");
-  else if (f.exponenta == 255 && f.mantissa !=0)
-    printf ("NaN")
+  else if (f.exponenta == max && f.mantissa !=0)
+    printf ("NaN");
   else 
     printf("%d * 2^%d * %f\n", f.sign, f.exponenta - 127, 1+((float)f.mantissa)/(1 << 23));
 }
 void value (int b)
 {
   int sign = (b >> 31); 
-  int exponenta = (b >> 23) & (1 << 8) - 1);
+  int exponenta = (b >> 23) & ((1 << 8) - 1);
   int mantissa = b & ((1 << 23) - 1);
   f.sign = sign;
   f.mantissa = mantissa;
   f.exponenta = exponenta;
+  print(f);
 }
 int main ()
 {
