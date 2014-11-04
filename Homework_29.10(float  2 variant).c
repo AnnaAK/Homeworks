6b39946460a2afa06726baf85e50502a1d7dd2b8
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-04 17:34:53 +0400

    Update and rename Homework_29.10(float variant 2).c to Homework_29.10(float  2 variant).c

---
+++ Homework_29.10(float  2 variant).c
@@ -0,0 +1,51 @@
+// вывести знак, экспоенту и мантиссу
+// с union
+#include <stdio.h>
+struct floatstruct
+{
+  int sign;
+  int exponenta;
+  int mantissa;
+} f;
+ void presentation (floatstruct)
+ #define max 255
+{
+  if (f.sign)
+  f.sign = -1;
+  else
+  f.sign = 1;
+  if (f.exponenta == 0 && f.mantissa == 0)
+    printf ("0\n");
+  else
+  if (f.exponenta == max && f.mantissa == 0)
+  if (f.sign>0)
+    printf ("sign = +");
+  else
+    printf ("sign = -");
+  else if (f.exponenta == max && f.mantissa !=0)
+    printf ("NaN");
+  else 
+    printf("%d * 2^%d * %f\n", f.sign, f.exponenta - 127, 1+((float)f.mantissa)/(1 << 23));
+}
+void value (int b)
+{
+  int sign = (b >> 31); 
+  int exponenta = (b >> 23) & ((1 << 8) - 1);
+  int mantissa = b & ((1 << 23) - 1);
+  f.sign = sign;
+  f.mantissa = mantissa;
+  f.exponenta = exponenta;
+  print(f);
+}
+int main (void)
+  {
+	union
+	{
+		float floatval;
+		int intval;
+	} floatN;
+
+	scanf("%f", &floatN.intval);
+	value(floatN.intval);
+
+}
