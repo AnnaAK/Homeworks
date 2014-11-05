57a7b0427b0e22a15a8b4febaa587fefbbe65dd2
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-05 15:50:48 +0400

    Update Homework_22.10 (binary representation).c

--- Homework_22.10 (binary representation).c
+++ Homework_22.10 (binary representation).c
@@ -1,15 +1,16 @@
+//Kudryashova Anna
 //двоичное представление числа
 
 #include <stdio.h>
  int main()
 {
- const int s = sizeof(int) * 8;
- int n=0, i = 0;
+  const int s = sizeof(int) * 8;
+  int n = 0, i = 0;
  scanf ("%d", &n);
  for (i = 0; i < s; i++)
  {
- printf ("%d", (n >> s - 1) & 1);
- n = n << 1;
+  printf ("%d", (n >> s - 1) & 1);
+  n = n << 1;
  }
  
 }
