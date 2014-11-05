ce5b9d116d7667c33a7985d596cc12da29146519
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-05 15:08:52 +0400

    Update Homework_05.11(buffer overflow).c

--- Homework_05.11(buffer overflow).c
+++ Homework_05.11(buffer overflow).c
@@ -1,1 +1,21 @@
 // переполнение буфера
+
+#include <stdio.h>
+#include <stdlib.h>
+int f2()
+{
+    printf("%s\n",f2);
+    exit (0);
+}
+int f1()
+{
+    printf("1\n");
+
+}
+void main()
+{
+char data [10];
+scanf("%s",data);
+int r=f1();
+printf ("%d\n",r);
+}
