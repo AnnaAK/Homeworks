2eae6c1676e64abdab35388f41486cb30b21a1eb
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-09 17:13:11 +0400

    Update Homework_22.10 (presentation).c
    выводить 1, если можно представить и 0, если нельзя

--- Homework_22.10 (presentation).c
+++ Homework_22.10 (presentation).c
@@ -1,1 +1,18 @@
-// Может ли целое число быть представлено на машине с n-разрядным словом и дополнительным кодом
+// Anna Kudryashova
+// Может ли целое число быть представлено на машине с n-разрядным словом и дополнительным кодом.
+
+#include <stdio.h>
+int presentation ( int n, int w)
+{
+   const int s = sizeof (int) * 8 + 1 + ~w;
+   int z = !(((n << s) >> s) ^ n);
+   return z;
+}
+main (void)
+{
+   int n = 0, w = 0;
+   scanf ("%d %d", &n, &w);
+   printf ("%d\n", presentation (n,w));
+   return 0;
+  
+}
