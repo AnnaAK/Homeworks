a9204784c8cf909ac29640e78a114564cd2cec19
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-09 21:03:29 +0400

    Create Homeworks_12.11(search of string).c

---
+++ Homeworks_12.11(search of string).c
@@ -0,0 +1,29 @@
+// Kudryashova Anna
+// сколько раз подстрока s2 входит в строку s1 
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char t[1000], s1[2000], s2[1000];
+	int a = strlen(s1), b = strlen(s2), i = 0, j = 0, k = 0, z = 0;
+	scanf("%s", &s1);
+	scanf("%s", &s2);
+	for (i = 0; i <= a - b; ++i)
+	{
+		k = 1;
+		for (j = 0; j < b; ++j)
+		{
+			if (s1[i + j] != s2[j])
+			{
+				k = 0;
+				break;
+			}
+		}
+		if (k)
+            z = z + 1;
+	}
+	printf("%d", z);
+	return 0;
+}
