af4c6da51ad8794bbde9d40691907c78f18c3549
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-11 21:35:14 +0400

    Update Homework_05.11(buffer overflow).c

--- Homework_05.11(buffer overflow).c
+++ Homework_05.11(buffer overflow).c
@@ -1,21 +1,47 @@
 // переполнение буфера
+// Kudryashova Anna
 
 #include <stdio.h>
 #include <stdlib.h>
 int f2()
-{
-    printf("%s\n",f2);
-    exit (0);
-}
-int f1()
-{
-    printf("1\n");
+ {
+    char *str = "COMPLITE";
+    char data[2];
+    strcpy(data, str);
 
-}
-void main()
+
+ }
+int f1()
+ {
+    printf(" COMPLITE\n");
+    exit (0);
+
+ }
+int main(void)
 {
-char data [10];
-scanf("%s",data);
-int r=f1();
-printf ("%d\n",r);
+   printf ("%d %d %d\n", &f2, &f1, &main);
+   f2();
+   return 0;
+}
+#include <stdio.h>
+#include <stdlib.h>
+int f2()
+ {
+    char *str = "COMPLITE";
+    char data[2];
+    scanf("%s", data);
+
+
+ }
+int f1()
+ {
+    printf(" COMPLITE\n");
+    exit (1);
+
+ }
+int main()
+{
+   printf ("%d %d %d\n", &f2, &f1, &main);
+   f2();
+   return 0;
 }
