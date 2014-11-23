ccf3cf8d0131a22e6cb779f8f3bb78b3d9ae6db5
AnnaAK <anna.kudryashova.va@gmail.com>
2014-11-23 15:24:00 +0400

    Update Homeworks_29.10(float 3 variant).c

--- Homeworks_29.10(float 3 variant).c
+++ Homeworks_29.10(float 3 variant).c
@@ -11,10 +11,8 @@ struct floatstruct
  void presentation (floatstruct)
  #define max 255
 {
-  if (f.sign)
-  f.sign = -1;
-  else
-  f.sign = 1;
+  if (f.sign) f.sign = -1;
+  else f.sign = 1;
   if (f.exponenta == 0 && f.mantissa == 0)
     printf ("0\n");
   else
@@ -23,7 +21,7 @@ struct floatstruct
     printf ("+ infinity");
   else
     printf ("- infinity");
-  else if (f.exponenta == max && f.mantissa !=0)
+  else if (f.exponenta == max && f.mantissa != 0)
     printf ("NaN");
   else 
     printf("%d * 2^%d * %f\n", f.sign, f.exponenta - 127, 1+((float)f.mantissa)/(1 << 23));
