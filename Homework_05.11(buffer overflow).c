f94d2e5f2d90a47edef924fe81ff6afd7081a79f
AnnaAK <anna.kudryashova.va@gmail.com>
2014-12-10 14:00:05 +0400

    Update Homework_05.11(buffer overflow).c

--- Homework_05.11(buffer overflow).c
+++ Homework_05.11(buffer overflow).c
@@ -2,11 +2,13 @@
 // Kudryashova Anna
 
 #include <stdio.h>
-int f2()
+#include <string.h>
+int f2(char overflow[])
  {
-    char data[2];
+    char data[4];
     printf ("\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n"); // stack до ввода данных
-    scanf ("%s", data);
+    strcpy (data,overflow);
+	printf("\n");
     printf ("\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n\t%p\n"); // stack после ввода данных
     return;
  }
@@ -18,8 +20,9 @@ int f1()
  }
 int main()
 {
+   char overflow[] = "overflowover\x59\x11\x87\x00";
    printf ("f2:\t%p\nf1:\t%p\nmain:\t%p\n", &f2, &f1, &main);
    printf("\n");
-   f2();
+   f2(overflow);
    return 0;
 }
